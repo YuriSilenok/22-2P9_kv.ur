26b8a7bfc2259d5549dcc3b323b8e79107ed48f9
Student <e442116@gmail.com>
2025-05-07 14:21:09 +0300

    Исправление Flake8

--- models.py
+++ models.py
@@ -8,11 +8,9 @@ db = SqliteDatabase("base.db")
 class Table(Model):
     """Класс BaseModel для хранения данных"""
 
-
     class Meta:
         """Класс Meta с настройками модели"""
         database = db
-
 
 class Kvur(Table):
     """Класс kvur"""
