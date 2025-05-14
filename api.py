ad66b6fb32a886bd869364ca2e84d052f6941f42
olechka4 <ol7278468@gmail.com>
2025-05-14 09:34:29 +0300

    Fix type

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 @app.get("/quadratic_equation")
-async def quadratic_equation(a: int, b: int, c: int):
+async def quadratic_equation(a: float, b: float, c: float):
     """Endpoint для вычесления квадратного уравнения"""
     massage = kv_ur(a, b, c)
     return {"Ответ": massage}
