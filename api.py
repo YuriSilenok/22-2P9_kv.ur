1795094cf5c9b29682ccb2d129bf50c221543382
olechka4 <ol7278468@gmail.com>
2025-05-14 09:17:11 +0300

    Api

---
+++ api.py
@@ -0,0 +1,11 @@
+"""Endpoint для вычесления квадратного уравнения"""
+from fastapi import FastAPI
+from logic import kv_ur
+
+app = FastAPI()
+
+@app.get("/quadratic_equation")
+async def quadratic_equation(a: int, b: int, c: int):
+    """Endpoint для вычесления квадратного уравнения"""
+    massage = kv_ur(a, b, c)
+    return {"Ответ": massage}
