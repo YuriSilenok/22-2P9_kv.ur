4b14e1d9aabc239ebb06f16f52583509ea8959f7
olechka4 <ol7278468@gmail.com>
2025-05-14 09:27:24 +0300

    Fix Flake8

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from logic import kv_ur
 
 app = FastAPI()
 
+
 @app.get("/quadratic_equation")
 async def quadratic_equation(a: int, b: int, c: int):
     """Endpoint для вычесления квадратного уравнения"""
